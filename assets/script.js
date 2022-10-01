8324e002087beab3811159637cec6887f2913d04
yasmine <yasmineashoush5@gmail.com>
2022-10-01 01:15:24 -0400

    <added features to script.js

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+{
+for (var x = 0; x < topics.length; x++) {
+ console.log(topics[x]);
+}
+}
+
+function selectTopic() {
+if (randomTopic === 'HTML') {
+ console.log("Let's study HTML!");
+} else if (if (randomTopic === 'HTML') {
+    === 'CSS') {
+ console.log("Let's study CSS!");
+} else if (if (randomTopic === 'HTML') {
+    === 'Git') {
+ console.log("Let's study Git!");
+} else if (if (randomTopic === 'HTML') {
+    === 'JavaScript') {
+ console.log("Let's study JavaScript!");
+} else {
+ console.log('Please try again!');
+}
+listTopics()
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
+
